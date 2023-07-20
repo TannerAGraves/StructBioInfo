0656620e455bb2033633de0237c759079db1b0cb
Marco Uderzo <marco.uderzodj@gmail.com>
2023-07-20 17:33:57 +0200

    completed scripts for training and inference

--- contact_net.py
+++ contact_net.py
@@ -25,12 +25,17 @@ def perform_inference(model, input_pdb):
     
 
     pdb_list = PDBList()
-    pdb_list.retrieve_pdb_file(input_pdb, pdir="/data/output/")
-    logging.info(f'Downloaded PDB: {input_pdb}')
+    pdb_list.retrieve_pdb_file(input_pdb, pdir="data/output/")
     
-    os.system(f"python3 calc_features.py {input_pdb}.cif -out_dir data/output/") 
+    os.system(f"python3 calc_features.py data/output/{input_pdb}.cif -out_dir data/output/") 
 
-    df = pd.read_csv("data/output/" + input_pdb + ".tsv", sep='\t')
+    try:
+        df = pd.read_csv("data/output/" + input_pdb + ".tsv", sep='\t')
+        logging.info(f"Successfully loaded {input_pdb}.tsv")
+    except FileNotFoundError:
+        logging.ERROR(f'File {input_pdb}.tsv does not exist, exiting.')
+        exit()
+
 
     X = df[['s_up', 's_down', 's_phi', 's_psi', 's_a1', 's_a2', 's_a3', 's_a4', 's_a5', 't_up', 't_down', 't_phi', 't_psi', 't_a1', 't_a2', 't_a3', 't_a4', 't_a5']]
     X = X.apply(lambda x: x.fillna(x.mean()) if x.dtype.kind in 'biufc' else x)
@@ -39,9 +44,7 @@ def perform_inference(model, input_pdb):
     minMax.fit(X)
     X_scaled = minMax.transform(X)
 
-    #print(X_scaled)
-
-
+    logging.info("Predicting contacts...")
     y_pred = model.predict(X_scaled)
 
     X_scaled = pd.DataFrame(X_scaled)
@@ -59,7 +62,8 @@ def perform_inference(model, input_pdb):
         out_data = out_data.rename(columns={i: features[i]})
 
 
-    out_data.to_csv(f"{input_pdb}_contactnet_pred.csv") 
+    out_data.to_csv(f"data/output/{input_pdb}_pred.csv") 
+    logging.info(f"Generated prediction file {input_pdb}_pred.csv in data/output/ folder.")
 
 
 
@@ -67,7 +71,8 @@ def perform_inference(model, input_pdb):
 
 def main():
 
-    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.WARNING)
+    
+    logging.basicConfig(format='[ContactNet]: %(message)s', level=logging.INFO)
 
     args = parse_arguments()
 
@@ -76,20 +81,23 @@ def main():
     training_mode = args.train
 
 
-    contact_net_model = ContactNet(training_mode) # training_mode: bool
-
+    contactnet = ContactNet(training_mode) # training_mode: bool
 
     if inference_mode:
         if input_pdb == None:
             logging.warning("No PDB_id specified!")
             return
         
-        logging.info("Performing inference on PDB:", input_pdb)
-        perform_inference(contact_net_model, input_pdb)
+        logging.info("Loading Pretrained model...")
+        cn_model = contactnet.load_pretrained_model()
+        logging.info(f"Processing PDB: {input_pdb}")
+        perform_inference(cn_model, input_pdb)
         
     elif training_mode:
-        logging.info("Performing re-training of ContactNet")
-        contact_net_model.train_model()
+        logging.info("Performing Training of ContactNet")
+        trained_model, hist = contactnet.train_model()
+        logging.info("Saving model to /model folder.")
+        contactnet.save_model(trained_model)
        
     else:
         logging.warning("No action specified. Use either --inference or --train.")
--- model.py
+++ model.py
@@ -1,35 +1,41 @@
-from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.preprocessing import MinMaxScaler
+import os
+import logging
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.preprocessing import MinMaxScaler, label_binarize
 from sklearn.feature_selection import SelectFromModel
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_auc_score
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import InstanceHardnessThreshold
+from keras import models
 from keras.models import Sequential
 from keras.layers import Input, Dense, Dropout
 from keras.callbacks import EarlyStopping
 from keras.utils import to_categorical
-from keras import models
-import pandas as pd
-import numpy as np
-import os
-import logging
+
+
 
 
 class ContactNet: #maybe make this class a singleton pattern
 
-    def __init__(self, train: bool):
-        self.train = train
+    def __init__(self, train_mode: bool):
+        self.training_mode = train_mode # if False -> inference
 
     def load_pretrained_model(self):
-        return models.load_model('path/to/model/model.h5')
+        return models.load_model('model/model.keras')
     
     def save_model(self, mdl) -> None:
-        mdl.save('path/to/mdl')
+        mdl.save('model/model.keras')
 
 
 
-    def load_data_ring(path: str) -> pd.DataFrame:
+    def load_data_ring(self, path: str) -> pd.DataFrame:
         dfs = []
         for filename in os.listdir(path + 'data/features_ring'):
             if filename[-4:] == '.tsv':
@@ -38,24 +44,15 @@ class ContactNet: #maybe make this class a singleton pattern
 
         return df
 
-    def preprocess_data(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    def preprocess_data(self, df: pd.DataFrame):
         df = df[df.Interaction.notna()]
         contact_dict = {"HBOND": 0, "IONIC": 1, "PICATION": 2, "PIPISTACK": 3, "SSBOND": 4, "VDW": 5}
-        y = df['Interaction']
+        y = df['Interaction'].copy() # had to .copy
         cat_names = list(y.astype('category').cat.categories)
         y.replace(contact_dict, inplace=True)
         X = df[['s_up', 's_down', 's_phi', 's_psi', 's_a1', 's_a2', 's_a3', 's_a4', 's_a5', 't_up', 't_down', 't_phi', 't_psi', 't_a1', 't_a2', 't_a3', 't_a4', 't_a5']]
         X = X.apply(lambda x: x.fillna(x.mean()) if x.dtype.kind in 'biufc' else x)
         
-        return X, y, cat_names
-
-
-    def train_model(self, path):
-        df = self.load_data_ring(path)
-        X, y, cat_names = self.preprocess_data(df)
-
-        num_classes = len(cat_names)
-
         # Is it selecting all features? Useless?
         #feature_sel = SelectFromModel(LogisticRegression(max_iter=100000))
         #feature_sel.fit(X, y)
@@ -64,16 +61,10 @@ class ContactNet: #maybe make this class a singleton pattern
         minMax.fit(X)
         X_scaled = minMax.transform(X)
 
-        X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, stratify=y, test_size=0.1, random_state=100)
-        
-        kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=100)
+        return X_scaled, y, cat_names
+    
 
-        X_train = np.array(X_train)
-        y_train = np.array(y_train)
-        X_test = np.array(X_test)
-        y_test = np.array(y_test)
-
-
+    def balance_data(self, X_train, y_train):
         logging.info("Applying Undersampling...")
         undersample = InstanceHardnessThreshold(estimator=AdaBoostClassifier(),sampling_strategy={0:70000,5:80000})
         X_bal, y_bal = undersample.fit_resample(X_train, y_train)
@@ -81,4 +72,114 @@ class ContactNet: #maybe make this class a singleton pattern
         oversample = SMOTE(sampling_strategy={1:20000,3:10000,2:20000,4:10000})
         X_bal, y_bal = oversample.fit_resample(X_bal, y_bal)
 
+        return X_bal, y_bal
+    
+    
+
+    def init_model(self, input_dim, num_classes) -> Sequential:
+        model = Sequential()
+        model.add(Input(input_dim))
+        model.add(Dense(units=128, activation='relu', kernel_initializer="glorot_normal"))
+        #model.add(Dropout(0.5))
+        model.add(Dense(units=128, activation='relu', kernel_initializer="glorot_normal"))
+        #model.add(Dropout(0.5))
+        model.add(Dense(units=128, activation='relu', kernel_initializer="glorot_normal"))
+        #model.add(Dropout(0.5))
+        model.add(Dense(units=128, activation='relu', kernel_initializer="glorot_normal"))
+        #model.add(Dropout(0.5))
+        model.add(Dense(units=128, activation='relu', kernel_initializer="glorot_normal"))
+        #model.add(Dropout(0.5))
+        model.add(Dense(units=num_classes, activation='softmax', kernel_initializer="glorot_normal"))
+        
+        model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['AUC'])
+        
+        return model
+
+
+    def train(self, model, X_bal, y_bal, y_cat, kfold, early_stopping):
+        fold = 0
+        hist = []
+        for train_idx, val_idx in kfold.split(X_bal, y_bal):
+            fold += 1
+            print(f"Fold {fold}/10")
+            Xfold_train, Xfold_val = X_bal[train_idx], X_bal[val_idx]
+            yfold_cat_train, yfold_cat_val = y_cat[train_idx], y_cat[val_idx]
+            metrics = model.fit(Xfold_train, yfold_cat_train,
+                    validation_data=(Xfold_val, yfold_cat_val),
+                    epochs=500, verbose=1,
+                    batch_size=16000,
+                    callbacks=[early_stopping])
+            hist.append(metrics)
+        
+        return hist #model should already be passed by reference so I just return hist
+
+
+    def report(self, model, X_test, y_test, num_classes):
+        outputs = model.predict(X_test)
+        y_pred = np.argmax(outputs, axis=1)
+        y_true = y_test
+
+        accuracy = accuracy_score(y_true, y_pred)
+        logging.info(f"Accuracy: {accuracy}")
+
+        # Binarize labels and compute ROC AUC
+        y_true_bin = label_binarize(y_true, classes=np.arange(num_classes))
+        y_pred_bin = label_binarize(y_pred, classes=np.arange(num_classes))
+        auc = roc_auc_score(y_true_bin, y_pred_bin, average='macro', multi_class='ovr')
+        logging.info(f"AUC: {auc}")
+
+        report = classification_report(y_true, y_pred)
+        cm = confusion_matrix(y_true, y_pred)
+
+        # Plotting confusion matrix
+        plt.figure(figsize=(10, 8))
+        sns.heatmap(cm, annot=True, cmap="Blues", fmt="d")
+        plt.title("Confusion Matrix")
+        plt.xlabel("Predicted Labels")
+        plt.ylabel("True Labels")
+        plt.show()
+
+        return report
+
+
+    def train_model(self, path=''):
+        df = self.load_data_ring(path)
+        X_scaled, y, cat_names = self.preprocess_data(df)
+
+        input_dim = X_scaled.shape[1]
+        num_classes = len(cat_names)
+
+        X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, stratify=y, test_size=0.1, random_state=100)
+        
+        kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=100)
+
+
+
+        X_train = np.array(X_train)
+        y_train = np.array(y_train)
+        X_test = np.array(X_test)
+        y_test = np.array(y_test)
+
+        X_bal, y_bal = self.balance_data(X_train, y_train)
+
         y_cat = to_categorical(y_bal, num_classes)
+
+        model = self.init_model(input_dim, num_classes)
+
+        logging.info("Model summary:")
+        model.summary()
+
+        es = EarlyStopping(
+                       monitor='loss',
+                       mode='min',
+                       patience=5,
+                       min_delta=0.0001
+                       )
+        
+        hist = self.train(model, X_bal, y_bal, y_cat, kf, es)
+
+        self.report(model, X_test, y_test, num_classes)
+
+        return model, hist
+
+
